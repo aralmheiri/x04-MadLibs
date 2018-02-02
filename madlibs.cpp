2276605a94aae04d1b08aa86f781575854a90c26
aralmheiri <aralmheiri@mail.csuchico.edu>
2018-02-01 19:45:15 -0800

    my story

--- madlibs.cpp
+++ madlibs.cpp
@@ -4,6 +4,20 @@
 
 int main()
 {
+int number;
+char grade;
+double money;
+std::string car;
+std::cout<<"enter a number ";
+std::cin>>number;
+std::cout<<"enter a charecter ";
+std::cin>>grade;
+std::cout<<"enter a vehicle ";
+std::cin>>car;
+std::cout<<"enter a price ";
+std::cin>>money;
+
+std::cout<<"i woke up at " << number << " AM then i got " << grade << " my " << car << " , and put " << money << " $ worth of gas."<<std::endl; 
 
   return 0;
 }
